1748eaf88360c21bf0fb8909f4ce8f5e428de478
Matt Klein <mklein246@gmail.com>
2019-05-29 14:33:40 -0700

    mvp success!

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code away!
+// const express = require('express');
+
+const server = require('./server.js')
+
+const userRouter = require('./users/userRouter.js');
+
+const postRouter = require('./posts/postRouter.js');
+
+server.use('/posts', postRouter)
+
+server.use('/users', userRouter)
+
+server.listen(5000, () => console.log("\nlistening on 5000\n"))
