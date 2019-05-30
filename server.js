3ed753ff76bd492e7f0ba5614ede040b7f1877a8
Matt Klein <mklein246@gmail.com>
2019-05-30 13:27:37 -0700

    changed cors thing back

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const express = require('express');
 
 const server = express();
 
-server.use(logger);
-server.use(useCORS);
+server.use(logger)
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
-function userCORS(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-};
 
 function logger(req, res, next) {
   console.log("type:", req.method);
