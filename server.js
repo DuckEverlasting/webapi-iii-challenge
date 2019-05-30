05a94e9843c54153153084002600e66eaad49d6a
Matt Klein <mklein246@gmail.com>
2019-05-30 13:12:25 -0700

    changed useCORS

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 
 const server = express();
 
-server.use(logger)
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+server.use(logger);
+server.use(useCORS);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
+
+function userCORS(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+};
 
 function logger(req, res, next) {
   console.log("type:", req.method);
